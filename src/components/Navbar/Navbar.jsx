d94c99b726f9892ac869ce471830831fae5f13c1
imshubhampatel <shubhampatel2024@gmail.com>
2022-06-02 15:48:23 +0530

    feat : added responsive to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,21 +6,17 @@ export default function Navbar() {
   return (
     <>
       <div className={styles.navbar}>
-        <div className={styles.navigation}>
-          <div className={`${styles.left} ${styles.half}`}>
-            <div className={`${styles.image} ${styles.logo}`}>
-              <img src={logo} alt="collge logo" />
-            </div>
-          </div>
-          <div className={`${styles.right} ${styles.half}`}>
-            <div className={`${styles.text_college} ${styles.text}`}>
-              <a href="https://btirt.ac.in">
-                <span >
-                  Babulal Tarabai Institute of Research and Technology, Sironja
-                  Sagar (M.P.)
-                </span>
-              </a>
-            </div>
+        <div className={`${styles.left} ${styles.half}`}>
+          <img src={logo} alt="collge logo" />
+        </div>
+        <div className={`${styles.right} ${styles.half}`}>
+          <div className={`${styles.text_college} ${styles.text}`}>
+            <a href="https://btirt.ac.in">
+              <span>
+                Babulal Tarabai Institute of Research and Technology, Sironja
+                Sagar (M.P.)
+              </span>
+            </a>
           </div>
         </div>
       </div>
